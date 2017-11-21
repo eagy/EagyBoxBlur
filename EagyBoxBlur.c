aa3afed334ff9221caacab2c402342815769db6b
Daniel Eagy <daniel.eagy@gmail.com>
2017-11-21 13:31:26 -0700

    Struct Update

--- EagyBoxBlur.c
+++ EagyBoxBlur.c
@@ -62,6 +62,13 @@ struct Pixel {
     unsigned int blue;
     unsigned int green;
 };
+
+typedef struct {
+    struct BMP_Header header;
+    struct DIB_Header dib;
+    struct Pixel **pixels;
+    int padding;
+}BMP;
 ////////////////////////////////////////////////////////////////////////////////
 //MAIN PROGRAM CODE
 
@@ -70,6 +77,8 @@ struct Pixel **origPixelMatrix;
 struct Pixel **newPixelMatrix;
 struct BMP_Header *header;
 struct DIB_Header *dib;
+BMP *in_bmp;
+BMP *out_bmp;
 
 //TODO: finish me
 void write_pixel_data(FILE *file);
@@ -84,6 +93,8 @@ void main(int argc, char *argv[]) {
     //TODO: finish me
     header = (struct BMP_Header *) malloc(sizeof(struct BMP_Header));
     dib = (struct DIB_Header *) malloc(sizeof(struct DIB_Header));
+    in_bmp = (BMP*)malloc(sizeof(BMP));
+    out_bmp = (BMP*)malloc(sizeof(BMP));
 
     //User Prompt
     char *buff;
@@ -96,10 +107,22 @@ void main(int argc, char *argv[]) {
     //Read in header info
     read_header_info(file, header, dib);
 
+    printf("1\n");
+    //Setting BMP pixel matrix
+    in_bmp->pixels = (struct Pixel **) malloc(sizeof(struct Pixel*) * in_bmp->dib.height);
+    int i;
+    for (i = 0; i < in_bmp->dib.height; i++)
+        in_bmp->pixels[i] = (struct Pixel *) malloc(sizeof(struct Pixel) * in_bmp->dib.width);
+
+    //Setting BMP pixel matrix
+    printf("1\n");
+    out_bmp->pixels = (struct Pixel **) malloc(sizeof(struct Pixe*) *out_bmp->dib.height);
+    for (i = 0; i < in_bmp->dib.height; i++)
+        out_bmp->pixels[i] = (struct Pixel *) malloc(sizeof(struct Pixel) * out_bmp->dib.width);
+
     //initialize origPixel matrix
     origPixelMatrix = (struct Pixel **) malloc(sizeof(struct Pixel *) * dib->height);
 
-    int i;
     for (i = 0; i < dib->height; i++)
         origPixelMatrix[i] = (struct Pixel *) malloc(sizeof(struct Pixel) * dib->width);
 
@@ -109,7 +132,7 @@ void main(int argc, char *argv[]) {
     for (i = 0; i < dib->height; i++)
         newPixelMatrix[i] = (struct Pixel *) malloc(sizeof(struct Pixel) * dib->width);
 
-    //Print Header
+/*    //Print Header
     printf("signature: %c%c\n", header->signature[0], header->signature[1]);
     printf("size: %d\n", header->size);
     printf("reserved1: %d\n", header->reserved1);
@@ -128,7 +151,7 @@ void main(int argc, char *argv[]) {
     printf("X Pixels Per Meter: %d\n", dib->xPixPerMeter);
     printf("Y Pixels Per Meter: %d\n", dib->yPixPerMeter);
     printf("Colors in Color Table: %d\n", dib->colorsInTable);
-    printf("Important Color Count: %d\n", dib->importantColors);
+    printf("Important Color Count: %d\n", dib->importantColors);*/
 
     short row, col;
     short value;
@@ -144,18 +167,20 @@ void main(int argc, char *argv[]) {
     scanf("%s", buff);
 
     FILE *out = fopen(buff, "wb");
-    write_pixel_data(out);
+    write_pixel_data(file);
 
 
     free(header);
     free(dib);
     free(origPixelMatrix);
+    free(newPixelMatrix);
     fclose(out);
     fclose(file);
 }
 
 void write_pixel_data(FILE *file) {
-    fwrite(&header->signature[0], sizeof(char), 1, file);
+    fwrite(&in_bmp, sizeof(BMP), 1, file);
+/*    fwrite(&header->signature[0], sizeof(char), 1, file);
     fwrite(&header->signature[1], sizeof(char), 1, file);
     fwrite(&header->size, sizeof(int), 1, file);
     fwrite(&header->reserved1, sizeof(short), 1, file);
@@ -175,48 +200,52 @@ void write_pixel_data(FILE *file) {
     fwrite(&dib->colorsInTable, sizeof(int), 1, file);
     fwrite(&dib->importantColors, sizeof(int), 1, file);
 
-    int row, col, value;
+    int row, col, value = 00;
     for (row = 0; row < (dib->height); row++) {
         for (col = 0; col < (dib->width); col++) {
             fwrite(&newPixelMatrix[row][col].blue, 1, 1, file);
             fwrite(&newPixelMatrix[row][col].green, 1, 1, file);
             fwrite(&newPixelMatrix[row][col].red, 1, 1, file);
         }
-        fwrite(&value, 2, 1, file);
-    }
+        fwrite(&value, 1, 2, file);
+    }*/
 }
 
 void read_header_info(FILE *file, struct BMP_Header *header, struct DIB_Header *dib) {
     //read bitmap file header (14 bytes)
-    fread(&header->signature, sizeof(char) * 2, 1, file);
-    fread(&header->size, sizeof(int), 1, file);
-    fread(&header->reserved1, sizeof(short), 1, file);
-    fread(&header->reserved2, sizeof(short), 1, file);
-    fread(&header->offset_pixel_array, sizeof(int), 1, file);
+    fread(&in_bmp->header.signature, sizeof(char) * 2, 1, file);
+    fread(&in_bmp->header.size, sizeof(int), 1, file);
+    fread(&in_bmp->header.reserved1, sizeof(short), 1, file);
+    fread(&in_bmp->header.reserved2, sizeof(short), 1, file);
+    fread(&in_bmp->header.offset_pixel_array, sizeof(int), 1, file);
 
     //read DIB header
-    fread(&dib->dibHeaderSize, sizeof(int), 1, file);
-    fread(&dib->width, sizeof(int), 1, file);
-    fread(&dib->height, sizeof(int), 1, file);
-    fread(&dib->planes, sizeof(short), 1, file);
-    fread(&dib->bitsPerPixel, sizeof(short), 1, file);
-    fread(&dib->compression, sizeof(int), 1, file);
-    fread(&dib->imageSize, sizeof(int), 1, file);
-    fread(&dib->xPixPerMeter, sizeof(int), 1, file);
-    fread(&dib->yPixPerMeter, sizeof(int), 1, file);
-    fread(&dib->colorsInTable, sizeof(int), 1, file);
-    fread(&dib->importantColors, sizeof(int), 1, file);
+    fread(&in_bmp->dib.dibHeaderSize, sizeof(int), 1, file);
+    fread(&in_bmp->dib.width, sizeof(int), 1, file);
+    fread(&in_bmp->dib.height, sizeof(int), 1, file);
+    fread(&in_bmp->dib.planes, sizeof(short), 1, file);
+    fread(&in_bmp->dib.bitsPerPixel, sizeof(short), 1, file);
+    fread(&in_bmp->dib.compression, sizeof(int), 1, file);
+    fread(&in_bmp->dib.imageSize, sizeof(int), 1, file);
+    fread(&in_bmp->dib.xPixPerMeter, sizeof(int), 1, file);
+    fread(&in_bmp->dib.yPixPerMeter, sizeof(int), 1, file);
+    fread(&in_bmp->dib.colorsInTable, sizeof(int), 1, file);
+    fread(&in_bmp->dib.importantColors, sizeof(int), 1, file);
+
+
+
 }
 
 void load_bmp(FILE *file) {
+
     int row, col;
     unsigned int padding;
-    for (row = 0; row < dib->height; row++) {
-        for (col = 0; col < (dib->width); col++) {
+    for (row = 0; row < in_bmp->dib.height; row++) {
+        for (col = 0; col < (in_bmp->dib.width); col++) {
 
-            fread(&origPixelMatrix[row][col].blue, 1, 1, file);
-            fread(&origPixelMatrix[row][col].green, 1, 1, file);
-            fread(&origPixelMatrix[row][col].red, 1, 1, file);
+            fread(&in_bmp->pixels[row][col].blue, 1, 1, file);
+            fread(&in_bmp->pixels[row][col].green, 1, 1, file);
+            fread(&in_bmp->pixels[row][col].red, 1, 1, file);
 
             //printf("%x, %x, %x, ", blue, green, red);
 
